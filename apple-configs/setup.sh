3cb1a042db0734e548868e7c4af3445a92cf21ac
Chris Hancock <chancock09@gmail.com>
2017-05-22 07:43:34 -0400

    Show hidden files in finder

--- apple-configs/setup.sh
+++ apple-configs/setup.sh
@@ -4,5 +4,9 @@ defaults write com.apple.dock static-only -bool TRUE
 # Move dock to right
 defaults write com.apple.dock orientation right
 
+# Show hidden files in finder
+defaults write com.apple.finder AppleShowAllFiles -bool TRUE
+
 # Refresh dock
 killall Dock
+killall Finder
